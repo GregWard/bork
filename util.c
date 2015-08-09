de8123b05cded21a9c1a279f268c3b61eee480d4
Colin J. Mills <colin.j.mills96@gmail.com>
2015-08-08 22:07:58 -0400

    * Added buildBorkDirectory().

--- util.c
+++ util.c
@@ -4,24 +4,20 @@
  *
  * Holds all the utility functions for bork.
  *
- * 2015/07/30
- *
- *
  * Colin J. Mills
  *
- * BSD Licensed
+ * 2015/07/30
  *
  */
-
-#define NON_ZERO -1
-
+#include "bork.h"
 #include <err.h>
-#include <stdio.h>
+#include <pwd.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
 #include <malloc.h>
-
-
-void* allocateBlock(size_t blockSize);
+#include <unistd.h>
 
 /*
  *
@@ -45,31 +41,41 @@ void* allocateBlock(size_t blockSize)
     if (NULL == block)
     {
         /* errx(3) is a function included from err.h. (man errx) */
-        errx(NON_ZERO, "There was an error in allocating the block of memory");
+        errx(false, "There was an error in allocating the block of memory");
     }
 
     return block ;
 }
 
 
-/* Testing Harness for allocate block at the moment 
- *
- * TODO: Remove later.
- */
-int main(void)
+void buildBorkDirectory(char** pathBuffer)
 {
-    int i = 0;
+    /* Variable Definition */
+    int usersUID = 0;                       // The current users UID
+    struct password* pw             = {0}; // This is for incase the getenv(3) call fails.
+    char* homeDirectory             = NULL; // THe home directory of the current user
 
-    char* block = NULL;
+    /* Logic */
+    usersUID = getuid();
 
-    block = allocateBlock(1024);
-    strcpy(block, "hello");
-    printf("%s\n", block);
+    /* Get the home directory */
+    homeDirectory = allocateBlock(BLOCK_SIZE);
 
+    /* 
+     * This bit of code uses the getenv(3) call which is documented using "man getenv". It gets an environment variable
+     * in our case the HOME variable. If that fails then we get the current user identification number and within the password
+     * struct there is a home directory element.
+     */
+    homeDirectory = getenv("HOME");
 
+    if (homeDirectory == NULL)
+    {
+        homeDirectory = getpwuid(getuid())->pw_dir;
+    }
 
-   return i;
+    strncat(*pathBuffer, homeDirectory, BLOCK_SIZE);
+
+    sprintf(*pathBuffer, "%s/%s", homeDirectory, BORK_DIR);
 }
-
 
 
