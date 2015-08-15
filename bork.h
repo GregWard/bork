420245fc08fc3b58770b681d9e350f9ace82f293
Colin J. Mills <colin.j.mills96@gmail.com>
2015-08-14 21:06:33 -0400

    * Added the character structs

--- bork.h
+++ bork.h
@@ -18,12 +18,40 @@
 /*
  * Constants
  */
-#define            true   0 // Truthy values are 0
-#define            false -1 // Falsy values are  -1
-#define BLOCK_SIZE 81       // The block size is a kilobyte.
 
-/* BORK Constants */
-#define BORK_DIR ".bork" // The directory that bork uses for configuration.
+/* Global Constants */
+#define            true   0  // Truthy values are 0
+#define            false -1  // Falsy values are  -1
+#define BLOCK_SIZE 81        // The block size is a kilobyte.
+#define BORK_DIR ".bork"     // The directory that bork uses for configuration.
+#define BORK_FILE "log.bork" // The filename of the log file.
+
+
+/* character.c Constants */
+
+
+/* 
+ * Types
+ */
+
+/* character.c */
+struct skill
+{
+    int attack;  // The amount of health  a character has.
+    int defence; // The amount of defence a character has.
+    int courage; // The amount of courage a character has.
+};
+
+
+struct character
+{
+    char* race; // The characters race.
+    char* name; // The characters name.
+    int health; // The characters health.
+    struct skill skillSet; // The skillset of a character
+};
+
+
 
 
 
@@ -34,6 +62,10 @@
 
 /* bork.c */
 void initLog(FILE**); 
+
+/* character.c */
+void initCharacter(struct character*); 
+
 
 /* util.c */
 void* allocateBlock     (size_t); 
